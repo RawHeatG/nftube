3fa991de997b80f4c87f7f94b8cc00fcf5467927
Rohit Guati <rawheat.gulati@hotmail.com>
2021-04-21 22:14:36 +0530

    Design VideoPlayer

--- src/Contexts/data-context.js
+++ src/Contexts/data-context.js
@@ -8,43 +8,64 @@ export function DataProvider({children}) {
             videoId: "zpROwouRo_M",
             title: "NFTs Are Fueling a Boom in Digital Art. Here’s How They Work | WSJ",
             uploadedBy: "Wall Street Journal",
-            description: "Non-fungible tokens, or NFTs, have exploded onto the digital art scene this past year. Proponents say they are a way to make digital assets scarce, and therefore more valuable. WSJ explains how they work, and why skeptics question whether they’re built to last. Photo Illustration: Jacob Reynolds/WSJ"
+            description: "Non-fungible tokens, or NFTs, have exploded onto the digital art scene this past year. Proponents say they are a way to make digital assets scarce, and therefore more valuable. WSJ explains how they work, and why skeptics question whether they’re built to last. Photo Illustration: Jacob Reynolds/WSJ",
+            likes: 111,
+            views: 999,
+            subscribers: 42
         },
         {
             videoId: "uZ_9HXU67RY",
             title: "The NFT Bubble: Why 97% of All NFT Art Right Now Is a Bad Investment | The Outer Realm Podcast",
             uploadedBy: "GaryVee",
-            description: "There is no doubt in my mind that non-fungible tokens will be a part of our lives for a long time. The problem right now is because it is such a new concept for many, people are rushing in and making large investments without doing the proper research and education. Today’s episode is an interview that I did on the podcast “The Outer Realm“ hosted by  Eric Paul Rhodes. We talk about my macro overview of the NFT space, why I believe that I’m going to be involved in it for the rest of my life, and why I believe that 99% of current NFT projects won’t end up as remarkable investments. Enjoy! Let me know what you thought."
+            description: "There is no doubt in my mind that non-fungible tokens will be a part of our lives for a long time. The problem right now is because it is such a new concept for many, people are rushing in and making large investments without doing the proper research and education. Today’s episode is an interview that I did on the podcast “The Outer Realm“ hosted by  Eric Paul Rhodes. We talk about my macro overview of the NFT space, why I believe that I’m going to be involved in it for the rest of my life, and why I believe that 99% of current NFT projects won’t end up as remarkable investments. Enjoy! Let me know what you thought.",
+            likes: 111,
+            views: 999,
+            subscribers: 42
         },
         {
             videoId: "a8ww4aNlPQU",
             title: "What is an NFT? (Crypto Beginners)",
             uploadedBy: "Rhett / Mankind",
-            description: "So I'm going to describe what an NFT is in the most basic terms as I can. NFT stands for Non Fungible Token."
+            description: "So I'm going to describe what an NFT is in the most basic terms as I can. NFT stands for Non Fungible Token.",
+            likes: 111,
+            views: 999,
+            subscribers: 42
         },
         {
             videoId: "nTmF26NUZTA",
             title: "Beeple Explains The Absurdity Of NFTs | So Expensive",
             uploadedBy: "Business Inside",
-            description: "Mike Winkelmann, better known as Beeple, has sold the most expensive digital artwork in history. It’s part of an explosion in the market for NFTs — tokens that prove ownership of things like digital art that you can’t even touch."
+            description: "Mike Winkelmann, better known as Beeple, has sold the most expensive digital artwork in history. It’s part of an explosion in the market for NFTs — tokens that prove ownership of things like digital art that you can’t even touch.",
+            likes: 111,
+            views: 999,
+            subscribers: 42
         },
         {
             videoId: "9zTpJJoHmnE",
             title: "This MF sold some JPGs for $3.5M last weekend - Beeple Art Drop",
             uploadedBy: "Rhett / Mankind",
-            description: "So Beeple. This mother f*cker who many of us in the digital art world have know for a long time just put some of his images together in a collection, made a slide show, chucked a bar code on it, put it in a digital frame and included some hair in a jar that's not even his pubes. Put it up for sale and just made $3.5M dollars this weekend. What the F*ck!"
+            description: "So Beeple. This mother f*cker who many of us in the digital art world have know for a long time just put some of his images together in a collection, made a slide show, chucked a bar code on it, put it in a digital frame and included some hair in a jar that's not even his pubes. Put it up for sale and just made $3.5M dollars this weekend. What the F*ck!",
+            likes: 111,
+            views: 999,
+            subscribers: 42
         },
         {
             videoId: "Xdkkux6OxfM",
             title: "What Are NFTs and How Can They Be Used in Decentralized Finance? DEFI Explained",
             uploadedBy: "Finematics",
-            description: "So what are NFTs all about? And how can they be used in decentralized finance? You’ll find answers to these questions in this video."
+            description: "So what are NFTs all about? And how can they be used in decentralized finance? You’ll find answers to these questions in this video.",
+            likes: 111,
+            views: 999,
+            subscribers: 42
         },
         {
             videoId: "_V0-HGMB8SI",
             title: "NFTs - If You Don’t Know, Now You Know | The Daily Social Distancing Show",
             uploadedBy: "The Daily Show with Trevor Noah",
-            description: "NFTs, or nonfungible tokens, are a new craze breaking out in the world of cryptocurrency, but what are you actually buying? #DailyShow​ #TrevorNoah​ #NFTs"
+            description: "NFTs, or nonfungible tokens, are a new craze breaking out in the world of cryptocurrency, but what are you actually buying? #DailyShow​ #TrevorNoah​ #NFTs",
+            likes: 111,
+            views: 999,
+            subscribers: 42
         }
 
     ];
--- src/Pages/VideoPlayer/VideoPlayer.js
+++ src/Pages/VideoPlayer/VideoPlayer.js
@@ -1,9 +1,45 @@
 import { useParams } from "react-router-dom";
+import { useData } from "../../Contexts";
 import "./VideoPlayer.css";
 
 export function VideoPlayer(){
     const { videoId } = useParams();
-    return {
-        
-    }
+    console.log("VideoId: ", videoId)
+    const { data } = useData();
+    const { title, description, uploadedBy, likes, views, subscribers } = data.find( item => item.videoId === videoId )
+    return (
+        <>
+            <div className="video-player">
+                <div className="video">
+                    <iframe width="100%" height="600px"
+                    src={`https://www.youtube.com/embed/${videoId}`}
+                    title={title} frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowfullscreen">
+                    </iframe>
+                </div>
+                <h1>{title}</h1>
+                <div className="engagements">
+                    <div class="inline">
+                        <p>Views: {views}</p>
+                        <p>Likes: {likes}</p>
+                    </div>
+                    <div>
+                        <button class="btn btn-secondary" >Like</button>
+                        <button class="btn btn-tertiary" >Watch Later</button>
+                    </div>
+                </div>
+                <div className="uploaded-by">
+                    <div>
+                        <h2>{uploadedBy}</h2>
+                        {/* <p>{subscribers} Subscribers</p> */}
+                    </div>
+                    <button class="btn btn-primary" >Subscribe</button>
+                </div>
+                
+                <div className="description">
+                    <p>{description}</p>
+                </div>
+            </div>
+        </>
+    )
 } 

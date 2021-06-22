afbcac7e9490c81a68f41335032b8f6a67aea7a1
Rohit Guati <rawheat.gulati@hotmail.com>
2021-06-22 19:11:51 +0530

    Style VideoPlayer

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
 import WhatshotRoundedIcon from "@material-ui/icons/WhatshotRounded";
-import SlowMotionVideoRoundedIcon from "@material-ui/icons/SlowMotionVideoRounded";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
 import PlaylistPlayRoundedIcon from "@material-ui/icons/PlaylistPlayRounded";
 import { useNavigate } from "react-router-dom";
 import "./Sidebar.css";
@@ -19,7 +19,7 @@ export function Sidebar() {
         Trending
       </div>
       <div onClick={() => navigate("/")} className="sidebar-item">
-        <SlowMotionVideoRoundedIcon />
+        <WatchLaterIcon />
         Watch Later
       </div>
       <div onClick={() => navigate("/playlist")} className="sidebar-item">
--- src/Pages/VideoPlayer/VideoPlayer.jsx
+++ src/Pages/VideoPlayer/VideoPlayer.jsx
@@ -1,6 +1,11 @@
 import { useParams } from "react-router-dom";
 import { Navbar, Sidebar } from "../../Components";
 import { useData } from "../../Contexts";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ThumbUpOutlinedIcon from "@material-ui/icons/ThumbUpOutlined";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import WatchLaterOutlinedIcon from "@material-ui/icons/WatchLaterOutlined";
+import PlaylistPlayRoundedIcon from "@material-ui/icons/PlaylistPlayRounded";
 import "./VideoPlayer.css";
 
 export function VideoPlayer() {
@@ -11,73 +16,79 @@ export function VideoPlayer() {
   const { title, description, uploadedBy, likes, views, subscribers } =
     data.find((item) => item.videoId === videoId);
   return (
-    <>
+    <div>
       <Navbar />
       <Sidebar />
-      <div className="video-player">
-        <div className="video">
-          <iframe
-            src={`https://www.youtube.com/embed/${videoId}`}
-            title={title}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen="allowfullscreen"
-          ></iframe>
+      {!data ? (
+        <div className="canvas">
+          <h1>Loading....</h1>
         </div>
-        <h1>{title}</h1>
-        <div className="engagements">
-          <div classname="inline">
-            <p>Views: {views}</p>
-            <p>Likes: {likes}</p>
-          </div>
-          <div>
-            <button
-              classname="btn btn-secondary"
-              onClick={() =>
-                dispatch({
-                  type: "REMOVE_FROM_PLAYLIST",
-                  payload: { playlistId: "liked", videoId: videoId },
-                })
-              }
-            >
-              Dislike
-            </button>
-            <button
-              classname="btn btn-secondary"
-              onClick={() =>
-                dispatch({
-                  type: "ADD_TO_PLAYLIST",
-                  payload: { playlistId: "liked", videoId: videoId },
-                })
-              }
-            >
-              Like
-            </button>
-            <button
-              classname="btn btn-tertiary"
-              onClick={() =>
-                dispatch({
-                  type: "ADD_TO_PLAYLIST",
-                  payload: { playlistId: "watched", videoId: videoId },
-                })
-              }
-            >
-              Watch Later
-            </button>
-          </div>
-        </div>
-        <div className="uploaded-by">
-          <div>
-            <h2>{uploadedBy}</h2>
-            <p>{subscribers} Subscribers</p>
-          </div>
-          <button classname="btn btn-primary">Subscribe</button>
-        </div>
+      ) : (
+        <div className="canvas">
+          <div className="video-player">
+            <div className="video">
+              <iframe
+                width="100%"
+                height="100%"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title={title}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen="allowfullscreen"
+              ></iframe>
+            </div>
+            <h1>{title}</h1>
+            <div className="engagements">
+              <div className="engagements-left">
+                <div>{views} views</div>
+                <div>{likes} likes</div>
+              </div>
+              <div className="engagements-right">
+                <ThumbUpOutlinedIcon
+                  onClick={() =>
+                    dispatch({
+                      type: "REMOVE_FROM_PLAYLIST",
+                      payload: { playlistId: "liked", videoId: videoId },
+                    })
+                  }
+                >
+                  Dislike
+                </ThumbUpOutlinedIcon>
+                <ThumbUpIcon
+                  onClick={() =>
+                    dispatch({
+                      type: "ADD_TO_PLAYLIST",
+                      payload: { playlistId: "liked", videoId: videoId },
+                    })
+                  }
+                >
+                  Like
+                </ThumbUpIcon>
+                <WatchLaterOutlinedIcon
+                  onClick={() =>
+                    dispatch({
+                      type: "ADD_TO_PLAYLIST",
+                      payload: { playlistId: "watched", videoId: videoId },
+                    })
+                  }
+                >
+                  Watch Later
+                </WatchLaterOutlinedIcon>
+              </div>
+            </div>
 
-        <div className="description">
-          <p>{description}</p>
+            <div className="uploaded-by">
+              <div>
+                <h2>{uploadedBy}</h2>
+                <div className="subscribers">{subscribers} Subscribers</div>
+              </div>
+            </div>
+            <div className="description">
+              <p>{description}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 }
